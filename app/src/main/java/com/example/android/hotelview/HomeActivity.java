4385ba22d01f6212b365e53892539cdf891c13c0
faris <ammar06faris@gmail.com>
2021-11-25 15:44:37 +0700

    commit yan

--- app/src/main/java/com/example/android/hotelview/HomeActivity.java
+++ app/src/main/java/com/example/android/hotelview/HomeActivity.java
@@ -14,7 +14,7 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        //lokasi splash screen beradaaaaa
+        //lokasi splash screen berada
 
         new Handler().postDelayed(new Runable () {
             @Override
--- app/src/main/java/com/example/android/hotelview/MainActivity.java
+++ app/src/main/java/com/example/android/hotelview/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -14,13 +15,27 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import cz.msebera.android.httpclient.Header;
+
+
+import com.example.android.hotelview.adapter.HotelAdapter;
+import com.example.android.hotelview.model.HotelModel;
+
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView mRecyclerView;
     private ArrayList<Hotel> mHotelsData;
-    private HotelsAdapter mAdapter;
+    ArrayList<HotelModel> hotelModels = new ArrayList<>();
+    private HotelAdapter hotelAdapter;
+    private HotelModel hotelModel;
+
 
 
     @Override
@@ -28,44 +43,94 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        hotelAdapter = new HotelAdapter();
         mRecyclerView = findViewById(R.id.recyclerView);
 
-        int gridColumnCount = getResources().getInteger(R.integer.grid_column_count);
-        mRecyclerView.setLayoutManager(new GridLayoutManager(this, gridColumnCount));
+        getUser();
 
-        mHotelsData = new ArrayList<>();
 
-        mAdapter = new HotelsAdapter(this, mHotelsData);
-        mRecyclerView.setAdapter(mAdapter);
 
-        initializeData();
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(linearLayoutManager); //vertikal
+
+        //menggabungkan recycleview dgn adapter
+        mRecyclerView.setAdapter(hotelAdapter);
+        //onClick
+        hotelAdapter.onClick(hotel -> {
+            Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+            intent.putExtra(DetailActivity.STRING_HOTEL, hotel);
+            startActivity(intent);
+        });
 
 
 
     }
 
+    private void getUser() {
+        AsyncHttpClient client = new AsyncHttpClient();
+        String url = "https://dev.farizdotid.com/api/purwakarta/hotel";
+        client.get(url, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                String respone = new String(responseBody);
+                try {
+                    parseJson(respone);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
 
-    private void initializeData() {
-        String[] hotelsList = getResources()
-                .getStringArray(R.array.hotels_titles);
-        String[] hotelsInfo = getResources()
-                .getStringArray(R.array.hotels_info);
-        TypedArray hotelsImageResources =
-                getResources().obtainTypedArray(R.array.hotels_images);
 
-        mHotelsData.clear();
+            }
 
-        for (int i = 0; i < hotelsList.length; i++) {
-            mHotelsData.add(new Hotel(hotelsList[i], hotelsInfo[i], hotelsImageResources.getResourceId(i, 0)));
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void parseJson(String respone) throws JSONException {
+        JSONObject jsonObject = new JSONObject(respone);
+        JSONArray dataArray =jsonObject.getJSONArray("hotel");
+
+        for (int i = 0; i < dataArray.length(); i++) {
+            JSONObject dataObject =dataArray.getJSONObject(i);
+            String nama_hotel =dataObject.getString("nama");
+            String alamat = dataObject.getString("alamat");
+            String no_telp =dataObject.getString("nomor_telp");
+            String gambar_url = dataObject.getString("gambar_url");
+
+            hotelModel = new HotelModel(nama_hotel,alamat,no_telp,gambar_url);
+            hotelModels.add(hotelModel);
         }
-        hotelsImageResources.recycle();
 
-        mAdapter.notifyDataSetChanged();
+        hotelAdapter.setData(hotelModels);
+
+
     }
 
-    public void resetSports(View view) {
-        initializeData();
-    }
+
+//    private void initializeData() {
+//        String[] hotelsList = getResources()
+//                .getStringArray(R.array.hotels_titles);
+//        String[] hotelsInfo = getResources()
+//                .getStringArray(R.array.hotels_info);
+//        TypedArray hotelsImageResources =
+//                getResources().obtainTypedArray(R.array.hotels_images);
+//
+//        mHotelsData.clear();
+//
+//        for (int i = 0; i < hotelsList.length; i++) {
+//            mHotelsData.add(new Hotel(hotelsList[i], hotelsInfo[i], hotelsImageResources.getResourceId(i, 0)));
+//        }
+//        hotelsImageResources.recycle();
+//
+//        mAdapter.notifyDataSetChanged();
+//    }
+
+//    public void resetSports(View view) {
+//        initializeData();
+//    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
