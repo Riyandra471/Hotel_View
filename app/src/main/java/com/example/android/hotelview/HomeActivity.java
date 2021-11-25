ee461c42c51f251af672c42309ba85f1fa8c6934
faris <ammar06faris@gmail.com>
2021-11-25 15:17:10 +0700

    test commit faris

--- app/src/main/java/com/example/android/hotelview/HomeActivity.java
+++ app/src/main/java/com/example/android/hotelview/HomeActivity.java
@@ -14,7 +14,7 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        //lokasi splash screen berada
+        //lokasi splash screen beradaaa
 
         new Handler().postDelayed(new Runable () {
             @Override
